39a9b10d0c21c8dc8c50e6b5c1e0d55461803443
muhammadraza <mr.muhammad.raza86@gmail.com>
2014-05-17 15:54:46 +0100

    push

--- src/main/java/uk/org/redalert/configuration/ApplicationStarter.java
+++ src/main/java/uk/org/redalert/configuration/ApplicationStarter.java
@@ -2,9 +2,8 @@ package uk.org.redalert.configuration;
 
 public class ApplicationStarter {
 
-    private static final int PORT = 8080;
     public static void main(String args[]) throws Exception{
-        new JettyServer(PORT, getContextPath()).startJetty();
+        new JettyServer(Integer.valueOf(System.getenv("PORT")), getContextPath()).startJetty();
 
     }
 
